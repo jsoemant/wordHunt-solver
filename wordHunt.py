59c04a66d68a2703a46cbfdcebcd3c7267d62cbe
Josh <jsoemant@uci.edu>
2022-04-06 02:58:39 -0700

    Cleaned up code and added tries

--- wordHunt.py
+++ wordHunt.py
@@ -1,98 +1,96 @@
 import time
 
-"""
-NOTE: This is the fast(er) wordHunt solver for reasons I detail in the README of the repo.
-This wordHunt solver is around 1.4x faster than the other one and depending on your computer that may factor
-in a few extra seconds, but in most cases I doubt it will make a real difference.
-"""
 
-# I build a word-checker set off of collins_words.txt to efficiently check for valid words
-d = set(line.strip() for line in open('collins_words.txt'))
+class Trie:
 
+    def __init__(self):
+        self.children = {}
 
-def parse_input() -> [[]]:
-    """Asks for letters and parses it into a relevant matrix"""
-    letters = input("Enter letters: ")
-    counter = 0
-    words = [[] for i in range(4)]
-    for i in range(16):
-        words[counter].append(letters[i].upper())
-        if (i + 1) % 4 == 0:
-            counter += 1
-    return words
+    def insert(self, word: str) -> None:
+        current = self.children
+        for i in word:
+            if i not in current:
+                current[i] = {}
+            current = current[i]
+        current["word"] = True
 
+    def search(self, word: str) -> bool:
+        current = self.children
+        for i in word:
+            if i not in current:
+                return False
+            current = current[i]
+        return "word" in current
 
-def get_words(words: [[]]) -> set:
-    """Starts looking for words in the matrix using the recursive search_strings func"""
-    result = set()
-    visited = [[False for i in range(len(words[0]))] for i in range(len(words))]
-    for row in range(len(words)):
-        for col in range(len(words)):
-            visited[row][col] = True
-            search_strings(row, col, words, result, visited, "")
-            visited[row][col] = False
-    return result
+    def startsWith(self, prefix: str) -> bool:
+        current = self.children
+        for i in prefix:
+            if i not in current:
+                return False
+            current = current[i]
+        return True
 
 
-def search_strings(row: int, col: int, words: [[]], result: set, visited: [[]], word: str) -> None:
-    """Uses DFS recursively to build new words and checks if they're valid or not"""
-    if len(word) >= 10:
-        return
-    word += words[row][col]
-    if len(word) >= 4:
-        if word in d:
-            result.add(word)
-    # top
-    if row != 0 and not visited[row - 1][col]:
-        visited[row - 1][col] = True
-        search_strings(row - 1, col, words, result, visited, word)
-        visited[row - 1][col] = False
-    # bottom
-    if row != len(words) - 1 and not visited[row + 1][col]:
-        visited[row + 1][col] = True
-        search_strings(row + 1, col, words, result, visited, word)
-        visited[row + 1][col] = False
-    # left
-    if col != 0 and not visited[row][col - 1]:
-        visited[row][col - 1] = True
-        search_strings(row, col - 1, words, result, visited, word)
-        visited[row][col - 1] = False
-    # right
-    if col != len(words[0]) - 1 and not visited[row][col + 1]:
-        visited[row][col + 1] = True
-        search_strings(row, col + 1, words, result, visited, word)
-        visited[row][col + 1] = False
-    # top left
-    if row != 0 and col != 0 and not visited[row - 1][col - 1]:
-        visited[row - 1][col - 1] = True
-        search_strings(row - 1, col - 1, words, result, visited, word)
-        visited[row - 1][col - 1] = False
-    # top right
-    if row != 0 and col != len(words[0]) - 1 and not visited[row - 1][col + 1]:
-        visited[row - 1][col + 1] = True
-        search_strings(row - 1, col + 1, words, result, visited, word)
-        visited[row - 1][col + 1] = False
-    # bottom left
-    if row != len(words) - 1 and col != 0 and not visited[row + 1][col - 1]:
-        visited[row + 1][col - 1] = True
-        search_strings(row + 1, col - 1, words, result, visited, word)
-        visited[row + 1][col - 1] = False
-    # bottom right
-    if row != len(words) - 1 and col != len(words[0]) - 1 and not visited[row + 1][col + 1]:
-        visited[row + 1][col + 1] = True
-        search_strings(row + 1, col + 1, words, result, visited, word)
-        visited[row + 1][col + 1] = False
+class WordHunt:
+
+    def __init__(self):
+        # I build a word-checker trie off of collins_words.txt to efficiently check for valid words
+        self._word_dict = Trie()
+        for line in open('collins_words.txt'):
+            self._word_dict.insert(line.strip())
+        self._visited = [[False for _ in range(4)] for i in range(4)]
+        self._w_matrix = self.parse_input()
+        self._directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+        self._result = set()
+
+    @staticmethod
+    def parse_input() -> [[]]:
+        """Asks for letters and parses it into a relevant matrix"""
+        letters = input("Enter letters: ")
+        counter = 0
+        words = [[] for _ in range(4)]
+        for i in range(16):
+            words[counter].append(letters[i].upper())
+            if (i + 1) % 4 == 0:
+                counter += 1
+        return words
+
+    def get_words(self) -> None:
+        """Starts looking for words in the matrix using the recursive search_strings func"""
+        for row in range(len(self._w_matrix)):
+            for col in range(len(self._w_matrix)):
+                self.search_strings(row, col, "")
+
+    def search_strings(self, row: int, col: int, word: str) -> None:
+        """Uses DFS recursively to build new words and checks if they're valid or not"""
+        if row < 0 or row >= 4 or col < 0 or col >= 4 or self._visited[row][col]:
+            return
+        word += self._w_matrix[row][col]
+        if self._word_dict.startsWith(word):
+            if self._word_dict.search(word):
+                self._result.add(word)
+        else:
+            return
+        self._visited[row][col] = True
+        for direction in self._directions:
+            self.search_strings(row + direction[0], col + direction[1], word)
+        self._visited[row][col] = False
+
+    def get_result(self) -> set:
+        return self._result
 
 
 def run():
-    """Runs all relevant functions together and prints out valid results"""
-    words = parse_input()
-    start = time.time()
-    found = get_words(words)
-    end = time.time()
-    for i in sorted(list(found), key=lambda x: len(x)):
-        print(i)
-    print("Seconds: ", (end - start))
+    """Runs all relevant functions together"""
+    while True:
+        word_hunt = WordHunt()
+        start = time.time()
+        word_hunt.get_words()
+        found = word_hunt.get_result()
+        end = time.time()
+        for i in sorted(found, key=lambda x: len(x)):
+            print(i)
+        print("Seconds: ", (end - start))
 
 
 if __name__ == '__main__':
